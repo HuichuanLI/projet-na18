38b22e89851ca620372edbfd50abe6c38f5b9395
huichuan li <lhc14124908@163.com>
2018-12-05 18:22:48 +0100

    add js and choose different type of user

--- sign.php
+++ sign.php
@@ -16,6 +16,10 @@ date_default_timezone_set("Europe/Paris");
 if(empty($_POST)) {
 	require(ROOT . '/view/front/sign.html');
 } else {
+	
+	if(empty($_POST['login']) || empty($_POST['nom']) || empty($_POST['prenom']) || empty($_POST['mail']) || empty($_POST['password1'])){
+		  header('Location: sign.php?result=empty');
+	}
 	$login = $_POST['login'];
 	$nom = $_POST['nom'];
 	$prenom = $_POST['prenom'];
@@ -23,29 +27,41 @@ if(empty($_POST)) {
 	$password = $_POST['password1'];
 	$panier_paye = 'true';
 	$code_promo = 'NULL';
-	$is_admin = 'false';
+	
 	$date = new DateTime();
 	$date_crea =$date->format('Y-m-d');
 
 
 	#creation utilisatuer
-	
+	$typeuser = $_POST['typeuser'];
+	if($typeuser == "admin"){
+		$is_admin = 'true';
+	}else{
+		$is_admin = 'false';
+	}
+
 
 	$sql = "INSERT INTO public.utilisateur(
-	login, mdp, nom, prénom, mail, date_creation_compte, est_admin, est_paye) VALUES ('$login','$password','$nom','$prenom','$mail','$date_crea',$is_admin,'$panier_paye')";
+	 login, mdp, nom, prénom, mail, date_creation_compte, est_admin, est_paye) VALUES ('$login','$password','$nom','$prenom','$mail','$date_crea',$is_admin,'$panier_paye')";
+	
 	$result = mConn()->prepare($sql);
 	$row = $result->execute();
-
+	
+	# when the user is vendeur
+	if($typeuser == "vendeur"){
+		$description = $_POST["description"];
+		$siret = $_POST["siret"];
+		$nom_magasin = $_POST["magasin"];
+		$sql = "INSERT INTO public.vendeur(login, description, siret, nom_magasin) VALUES ('$login', '$description', '$siret', '$nom_magasin');";
+		$result = mConn()->prepare($sql);
+		$row = $result->execute();
+	}
 
 	if ($row) {
-	  header('Location: log.php');
+	  	header('Location: log.php');
 	}
 	else {
-
-		echo 'Erreur lors de la création';
-		echo "\nPDO::errorInfo():\n";
-		print_r($result->errorInfo());
-
+		 header('Location: sign.php?result=existe');
 	}
 }
 
