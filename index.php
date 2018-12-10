a3101fb456231d9c8b8a4417ab6c2052a3bea6af
huichuan li <lhc14124908@163.com>
2018-12-10 21:14:12 +0100

    change style of  index page

--- index.php
+++ index.php
@@ -16,14 +16,12 @@ $sql = "SELECT ref_produit,nom_produit FROM public.produit";
 $result  = mQuery($sql);
 //requête qui gère l'achat
 
-
   if (isset($_POST['buy'])){
     $reprod = $_POST['buy'];
     $login_acheteur = $_SESSION['login'];
 
     $sql2 = "INSERT INTO produit_est_dans_le_panier VALUES
     ('$reprod','$login_acheteur')";
-
     $result = mConn()->prepare($sql2);
     $retour = $result->execute();
   }
