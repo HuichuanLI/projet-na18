9deb0576c3d32a2ac2773cabe33f9097500f6a1f
huichuan li <lhc14124908@163.com>
2018-12-10 18:06:39 +0100

    bug

--- index.php
+++ index.php
@@ -20,8 +20,7 @@ if(!empty($_SESSION['login'])){
   $reprod = isset($_POST['buy']);
   $login_acheteur = $_SESSION['login'];
 
-  $sql2 = "INSERT INTO UTILISATEUR_ACHETE_PRODUIT VALUES
-  ('$reprod', '$login_acheteur')";
+  $sql2 = "INSERT INTO UTILISATEUR_ACHETE_PRODUIT VALUES ('$reprod', '$login_acheteur')";
 
   $result = mConn()->prepare($sql2);
   $retour = $result->execute();
