d72a4117f7584ede5fe5641e613aff93c9523254
Vladimir Tchekovy <vlad1998.18.5@gmail.com>
2018-12-11 20:21:41 +0100

    supprimer + panier

--- index.php
+++ index.php
@@ -16,16 +16,25 @@ $sql = "SELECT ref_produit,nom_produit FROM public.produit";
 $result  = mQuery($sql);
 //requête qui gère l'achat
 
-  if (isset($_POST['buy'])){
-    $reprod = $_POST['buy'];
-    $login_acheteur = $_SESSION['login'];
+if (isset($_POST['buy'])){
+  $reprod = $_POST['buy'];
+  $login_acheteur = $_SESSION['login'];
+  if(isset($_POST['qt'])){
     $qt = $_POST['qt'];
-
-    $sql2 = "INSERT INTO produit_est_dans_le_panier VALUES
-    ('$reprod','$login_acheteur','$qt')";
-
-    $result = mExec($sql2);
   }
+
+
+
+
+
+
+  $sql2 = "INSERT INTO produit_est_dans_le_panier VALUES
+  ('$reprod','$login_acheteur','$qt')";
+
+  $result = mExec($sql2);
+
+
+}
 
 
 require('./view/front/index.html');
