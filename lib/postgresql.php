be2b5b4fbf1be7acb1bfb4b2ad0715014ceb08d3
huichuan li <lhc14124908@163.com>
2018-12-14 17:17:30 +0100

    version

---
+++ lib/postgresql.php
@@ -0,0 +1,53 @@
+<?php
+/***
+*index.php front-page
+*
+*@author huichuan.li
+*@link https://github.com/HuichuanLI
+*@since 2017.6
+*@copyright Gpl
+*/
+//connect to the sql
+
+error_reporting(E_ALL ^ E_DEPRECATED);
+
+function mConn() {
+  static $connexion = null;
+  if($connexion === null) {
+    $connexion = new PDO('pgsql:host=db;port=5432;dbname=yyh', 'yyh', 'haha1sbccy');
+  }
+
+  return $connexion;
+}
+
+
+//select function
+function mQuery($sql){
+  
+  $connexion = mConn();
+  $resultset = $connexion->prepare($sql);
+  $resultset->execute();
+  $index = 0;
+
+  while ($row = $resultset->fetch(PDO::FETCH_ASSOC)) {
+    $result[$index] = $row;
+    $index++;
+  }
+  if(isset($result)){
+    return $result;
+  }else{
+    return null;
+  }
+}
+
+//insert function
+function mExec($sql){
+  $connexion = mConn();
+  $resultset = $connexion->prepare($sql);
+  if ($resultset->execute() == true) {
+   return "true";
+  }
+  else {
+    return "false";
+  }
+}
